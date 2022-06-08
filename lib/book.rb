1dd4900186b161fba6e8e9d8473fbdddf6634e7b
Mai Vang <vmaineng@gmail.com>
2022-06-07 17:25:02 -0700

    commit tree 08bc64d464d3590490a5359d809ac0b754a1359e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book
+    attr_reader :title, :author, :page_count, :genre
+    attr_accessor :turn_page
+
+    def initialize(title)
+        @title = title
+    end
+
+end
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition, :cobble
+
+    def initialize(brand)
+        @brand = brand
+    end
+end
+
+brand.new("Shoe has been repaired")
